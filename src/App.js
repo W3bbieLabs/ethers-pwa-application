0838a7d2bcb2c49654c886958b07aa878bdd7be7
Chris Crawforod <chris35469@gmail.com>
2023-09-25 22:07:31 -0500

    Add basic support for events using thirdweb

--- src/App.js
+++ src/App.js
@@ -1,34 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
-import { getAllPokemonList } from './api/pokemon';
+import { useEffect, useState, useRef } from 'react';
+import { useContractRead, useContract, useContractEvents } from "@thirdweb-dev/react";
+import { ethers } from "ethers";
+//import { getAllPokemonList } from './api/pokemon';
 
 function App() {
-  const [pokemonData,setPokemonData] = useState([]);
-  
-  useEffect(() => {
-    async function fetchData(){
-      const data = await getAllPokemonList();
-      setPokemonData(data?.results);
-    }
-    fetchData();
-  },[])
-  return (
-    <div style={{marginTop:'40px',justifyContent:'space-around',display:'flex',flexWrap:'wrap', width:'90%', margin:'auto'}}>
-      
-      {pokemonData?.map((poke,i) => {
-        return (
-          <div style={{width: '400px',height:'330px', border:'2px solid #000000', margin:'30px 10px'}}>
-              <div style={{padding:'5px 10px'}}>
-                <p style={{fontWeight:'bold',textTransform:'capitalize'}}> {poke.name}</p>
-              </div>
-              <img style={{height: '300px', width:'300px'}} alt="pokemon" src={`https://img.pokemondb.net/artwork/large/${poke.name}.jpg`}/>
-          </div>
-        )
-      })}
-      <div>
+  const [pokemonData, setPokemonData] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [contractAddress, setContractAddress] = useState("0xCF205808Ed36593aa40a44F10c7f7C2F67d4A4d4");
+  const initialized = useRef(false)
+  const { contract } = useContract(contractAddress);
+  const { data: trades } = useContractEvents(contract);
+  //const { data: totalSupply, isLoading, error } = useContractRead(contract, "totalSupply");
+  //const { data: name } = useContractRead(contract, "name");
 
+  // Your smart contract address
+  //const contractAddress = "0xA906EEf381Cf828f0F2Bfcc6e714a34Fd4e80399";
+
+  useEffect(() => {
+    async function testCommand() {
+      //console.log(contract)
+    }
+
+    if (!initialized.current) {
+      initialized.current = true
+      //testCommand()
+    }
+  }, [])
+
+  function getEth(hex) {
+    //return <span>{ethers.utils.formatUnits(hex) * (10 ** 18)}</span>
+    return <span>{ethers.utils.formatEther(hex)}</span>
+  }
+
+  function handleEvent(event) {
+    let { transaction, eventName, data } = event
+    let { transactionHash } = transaction
+    let { trader, subject, ethAmount } = data
+    //console.log(event)
+    //console.log(transactionHash)
+    return (
+      <div key={transactionHash} style={{ width: '400px', height: '330px', border: '2px solid #000000', margin: '30px 10px' }}>
+        <div style={{ padding: '5px 10px' }}>
+          <p style={{ fontWeight: 'bold', textTransform: 'capitalize' }}>
+            <span>{trader}</span>{"-->"}<span>{subject}</span>: {getEth(ethAmount._hex)}
+          </p>
+        </div>
+        {/*<img style={{height: '300px', width:'300px'}} alt="pokemon" src={`https://img.pokemondb.net/artwork/large/${poke.name}.jpg`}/> */}
       </div>
+    )
+  }
+
+  return (
+    <div style={{ marginTop: '40px', justifyContent: 'space-around', display: 'flex', flexWrap: 'wrap', width: '90%', margin: 'auto' }}>
+      <h1>Hello World</h1>
+      <h2>{contractAddress}</h2>
+      {trades?.map((event) => { return (handleEvent(event)) })}
+      {/*<button onClick={go}>Test</button>*/}
+      {/*totalSupply ? getTotalSupply() : <></>*/}
+      {/*name ? <h2>{name}</h2> : <></>*/}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import data from "./data/config.json";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { Base } from "@thirdweb-dev/chains";
+
+
+//import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThirdwebProvider activeChain={Base} clientId={data.client_id}>
+      <App />
+    </ThirdwebProvider>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
